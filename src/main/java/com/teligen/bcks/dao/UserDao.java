cf898f91306492e04e904a0b1c5b860bc1808deb
DismalSnail <yepenghaobit@gmail.com>
2021-07-26 22:30:25 +0800

    login verify 11%

--- src/main/java/com/teligen/bcks/dao/UserDao.java
+++ src/main/java/com/teligen/bcks/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.teligen.bcks.dao;
 
 import com.teligen.bcks.utils.SpringContextUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -13,19 +15,19 @@ import java.sql.SQLException;
  * @date 2021/7/25
  */
 public class UserDao {
-    private DataSource druidDataSource = (DataSource) SpringContextUtils.getContext().getBean("druidDataSource");
+    private final static Logger logger = LoggerFactory.getLogger(UserDao.class);
+    private final DataSource druidDataSource = (DataSource) SpringContextUtils.getContext().getBean("druidDataSource");
 
     public String getUserPassword(String account){
         String password = null;
         try {
             Connection connection = druidDataSource.getConnection();
-            System.out.println(connection.getSchema());
             PreparedStatement stmt = connection.prepareStatement("SELECT password FROM tb_user WHERE account = ?");
             stmt.setString(1,account);
             ResultSet resultSet = stmt.executeQuery();
             resultSet.next();
             password = resultSet.getString(1);
-
+            logger.error("password is {}",password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
